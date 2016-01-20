a0ed0277c7854f34ff86593e3c419795bb6c5ce4
pnitram <martin.pedersen@me.com>
2016-01-20 14:34:59 +0100

    Include db-tilkobling.php

--- oppgave11/meny.php
+++
@@ -1,8 +0,0 @@
-<h1>Øvingsopgave 11 - PHP</h1>
-<h2>Student: Martin Pedersen</h2>
-<h3>Brukerfunksjoner</h3>
-
-<ul>
-	<li><a href="./registrere-data.php">Registrere data</a></li>
-	<li><a href="./les-data.php">Vis data</a></li>
-</ul>
