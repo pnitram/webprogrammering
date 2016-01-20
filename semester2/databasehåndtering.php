159fbbf6f20224b90adaf182722e21cda3b50c61
pnitram <martin.pedersen@me.com>
2016-01-20 12:54:01 +0100

    databasehåndtering php

--- semester2/databasehåndtering.php
+++ semester2/databasehåndtering.php
@@ -28,7 +28,7 @@ $sqlResultat=mysqli_query($db,$sqlSetning) or die ("Ikke mulig å hente fra $dat
 $antallRader=mysqli_num_rows("sqlResultat");
 
 
-// html tabell --> print ("<table>; print("<th>postnr)
+// html tabell --> print ("<table>; print("<th>postnr osv)
 for ($r=1;$v<=$antallRader;$v++) {
 
 	$rad=mysqli_fetch_array($sqlResultat);
@@ -36,6 +36,57 @@ for ($r=1;$v<=$antallRader;$v++) {
 	$poststed=$rad["poststed"];
 	print("$postnr $poststed </br>");
 }
+?>
+
+<?php
+/*
+<form .....>
+postnr <input .... osv>
+*/
+
+$_POST["postnr"];
+$_POST["poststed"];
+if (!$postnr || !$poststed) {
+	print ("Begge må fylles ut");
+}
+
+else {
+
+	include("db_tilkobling.php");
+	$sqlSetning="SELECT * FROM poststed WHERE postnr='$postnr';";
+
+	$sqlResultat=mysqli_query($db,$sqlSetning) or die ("Ikke mulig å hente fra $database: " .mysql_error() );
+
+	$antallRader=mysqli_num_rows("sqlResultat");
+
+	if ($antallRader!"=0") {
+		print("Poststed er registrert fra før");
+	}
+
+	else
+{
+/*
+
+SJEKKE OM POSTNR FINNES  I POSTSTED BASEN
+
+KJØRE EN  IF ($antallRader==0) {
+	
+	print("Postnr finnes ikke i poststedtabellen");
+}
+
+
+*/
+
+	$sqlSetning="INSERT INTO poststed VALUES('$postnr','$poststed');";
+	mysqli_query($db,$sqlSetning) or die ("Ikke mulig å hente fra $database: " .mysql_error() );
+	print("Poststed registrert");	
+
+}
+
+
+}
+
+
 
 
 ?>
